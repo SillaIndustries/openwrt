a80f4f9802871852c0595fad8e9002e635e16b31
Giovanni Giacobbi <giovanni@giacobbi.net>
2020-10-07 11:26:22 +0000

    Updated build script for dynamic files package plus version data fix

--- scripts/prism-full-build.sh
+++ scripts/prism-full-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-FILES_PACK="files-prism-1.4t-20200921"
-
 set -eu
 
 out() {
@@ -12,6 +10,7 @@ out() {
 cpus=$(($(nproc) + 1))
 out "Starting FULL build process using $cpus processes"
 
+##############################################################################
 
 out "1/5) Updating feeds"
 ./scripts/feeds update
@@ -20,27 +19,33 @@ if [ -e "files" ]
 then
   out "Skipping 'files' directory creation (already existing)"
 else
-  out "Creating 'files' directory from feeds..."
+  FILES_PACK=$(cat feeds/prism/prism/prism-files/CURRENT)
+  out "Creating 'files' directory from '$FILES_PACK'..."
   tar zxvf feeds/prism/prism/prism-files/src/$FILES_PACK.tar.gz
   ln -sf $FILES_PACK files
 fi
 
+##############################################################################
 
 out "2/5) Installing feeds"
 ./scripts/feeds install -a
 
+##############################################################################
 
 out "3/5) Applying Prism config"
 cp prism.config .config
 make defconfig
 
+##############################################################################
 
 out "4/5) Downloading packages"
 make download
 
+##############################################################################
 
 out "5/5) Building..."
 make -j$cpus world
 
+##############################################################################
 
 out "DONE."
